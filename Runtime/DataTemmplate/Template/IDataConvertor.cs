a40b6471c2c1235e2ea60f6f3ee777948de93614
DarkNight <truonguit2010@gmail.com>
2021-06-28 06:56:11 +0700

    - Add more Utility Interface

---
+++ Runtime/DataTemmplate/Template/IDataConvertor.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public interface IDataConvertor<F, T>
+{
+    T From(F f);
+
+    List<T> From(List<F> fs);
+}
---
+++ Runtime/DataTemmplate/Template/IMasterDataService.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public interface IMasterDataService<T> : IService
+{
+    T Get(string id);
+
+    List<T> GetAll();
+}
